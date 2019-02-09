c695f7a4445682494e563cd904144e5fba0c1ab0
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-09 01:52:33 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,19 @@
-def turn_count
+def turn_count(board)
   count = 0
   board.each do |token|
     if token == " X " || " O "
       count += 1 
     end
   end
+  count
 end
+
+def current_player(board)
+  count = turn_count(board)
+  if count.even 
+    "X"
+  else
+    "O"
+  end
+end
+  
